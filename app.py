bc7e6a617289043545976da3809a7f1499ebe03a
ThisIsFarhan <farhanak128@gmail.com>
2024-07-13 20:00:05 +0500

    added

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ st.title("Extractive Text Summarizer Using NLP")
 st.write("Developed and Deployed by Farhan Ali Khan")
 st.divider()
 text = st.text_input("Enter the text")
-percentage = st.slider("Percentage",1,100)
+# percentage = st.slider("Percentage",1,100)
 if len(text) != 0:
     st.text_area("Text", text)
 btn = st.button("Summarize")
 st.divider()
 if len(text) != 0 and btn == 1:
-    st.text_area("Summary", summarize(text, " ", percentage))
+    st.text_area("Summary", summarize(text, " ", 75))
